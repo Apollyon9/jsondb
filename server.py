770cfeec8c51958dba6da8ec09606ca4e63f1803
Kaneki <cool5tarXV@gmail.com>
2021-07-29 09:21:15 +0800

    working read/write time to make api

--- server.py
+++ server.py
@@ -2,41 +2,61 @@ from flask import Flask, render_template, request
 from threading import Thread
 import random
 import json
+import subprocess
+import os
+
+path = "db/"
 
 code = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"]
 
-def loaddb(key):
+def loaddb(key,pwd):
   toreturn = ""
   with open("db.json") as f:
     db = json.load(f)
   try:
-    toreturn = db[str(key)]
+    folder = db[str(key)]
+    with open(f"{path}/{folder}/db.json", "r") as x:
+      db2 = json.load(x) 
   except KeyError:
     return "Key Error: Not Found"
-  return toreturn
+  return db2
 
+def runcmd(command):
+    subprocess.check_output(command,shell=True)
 
+def createdb(folder,pwd):
+	cl = 30
+	with open("db.json") as f:
+		db = json.load(f)
+	with open("usedcodes.json","r") as x:
+		us = json.load(x)
+	codex = ""
+	while cl != 0 or cl > 0:
+		codex += str(random.choice(code))
+		cl -= 1
+	while codex in us:
+		while cl != 0 or cl > 0:
+			codex += str(random.choice(code))
+			cl -= 1
+	us.append(codex)
+	db[str(codex)] = str(folder)
+	print("-> Creating database folder...")
+	for folders in os.listdir(f"{path}"):
+		if folders == folder:
+			return "db already exists"
+	runcmd(f"cd {path} && mkdir {folder}")
+	runcmd(f"touch {path}/{folder}/db.json")
+	runcmd(f"touch {path}/{folder}/pwd.json")
+	with open(f"{path}/{folder}/db.json","w") as aliasdb:
+		aliasdb.write("{}")
+	with open(f"{path}/{folder}/pwd.json","w") as pwddb:
+		pwddb.write(str({"pwd":f"{pwd}"}))
 
-def createdb():
-  with open("db.json") as f:
-    db = json.load(f)
-  with open("usedcodes.json","r") as x:
-    us = json.load(x)
-  codex = ""
-  while cl != 0 or cl > 0:
-    codex += str(choice(code))
-    cl -= 1
-  while codex in us:
-    while cl != 0 or cl > 0:
-      codex += str(choice(code))
-      cl -= 1
-  us.append(codex)
-  db[str(codex)] = {}
-  with open("db.json","w") as z:
-    json.dump(kg,z)
-  with open("usedcodes.json","w") as z:
-    json.dump(us,z)
-  return codex
+	with open("db.json","w") as z:
+		json.dump(db,z)
+	with open("usedcodes.json","w") as z:
+		json.dump(us,z)
+	return codex
   
 app = Flask('')
 
@@ -50,30 +70,53 @@ def home():
 
 @app.route("/key/")
 def key():
-  action = request.args.get("action")
-  keyx = request.args.get("key")
-  db = loaddb(keyx)
+  with open("db.json") as m:
+    maindb = json.load(m)
+  data = request.args.get("data")
+  data = json.loads(data)
+  key = data["key"]
+  pwd = data["pwd"]
+  folder = maindb[str(key)]
+  action = data["action"]
+  db = loaddb(key,pwd)
   if action == "read":
-	return db.text
+    return str(db)
   elif action == "write":
-	table = request.args.get("table")
-	value = request.args.get("value")
-	db[table] = [value]
-	with open("db.json","w") as z:
-		json.dump(db,z)
-	return "Success"
+    write = data["write"]
+    write = str(write)
+    write = write.replace("'",""" " """)
+    print(str(write))
+    #try:
+    writetest = json.loads(str(write))
+    #except:
+      #return "please enter in json format"
+    print("-> Writing data...")
+    runcmd(f"rm -rf {path}/{folder}/db.json")
+    runcmd(f"touch {path}/{folder}/db.json")
+    with open(f"{path}/{folder}/db.json","w") as writedb:
+      writedb.write(str(write))
+    return "success"
   else:
-	return "Please supply an action"
+    return "please supply an action"
 
+@app.route("/create/")
+def create():
+	data = request.args.get("data")
+	data = json.loads(data)
+	folder = data["folder"]
+	pwd = data["pwd"]
+	key = createdb(folder,pwd)
+	return str({"key": f"{key}"})
   
 
 def run():
     app.run(host='0.0.0.0', port=random.randint(2000, 9000))
 
 
-def keep_alive():
+def start():
     '''
 	Creates and starts new thread that runs the function run.
 	'''
     t = Thread(target=run)
     t.start()
+start()
